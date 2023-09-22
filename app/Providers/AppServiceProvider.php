0cadc93078456d049f590d38c6b3667e33758c61
Bogdan <bengemingg@gmail.com>
2023-09-22 20:28:29 +0300

    Jetstream+

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\Chats\Repositories\ChatRepository;
+use App\Services\Chats\Repositories\EloquentChatRepository;
+use App\Services\Messages\Repositories\EloquentMessageRepository;
+use App\Services\Messages\Repositories\MessageRepository;
+use App\Services\Users\Repositories\EloquentUserRepository;
+use App\Services\Users\Repositories\UserRepository;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(ChatRepository::class, EloquentChatRepository::class);
+        $this->app->bind(MessageRepository::class, EloquentMessageRepository::class);
+        $this->app->bind(UserRepository::class, EloquentUserRepository::class);
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
