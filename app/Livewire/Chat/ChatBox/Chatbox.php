67b1d167cfb21a2fedb09e91abfb52e89abd4735
Bogdan <bengemingg@gmail.com>
2023-10-20 14:47:39 +0300

    Fix loader

--- app/Livewire/Chat/ChatBox/Chatbox.php
+++ app/Livewire/Chat/ChatBox/Chatbox.php
@@ -2,7 +2,6 @@
 
 namespace App\Livewire\Chat\ChatBox;
 
-use App\Events\MessageRead;
 use App\Models\Chat;
 use App\Services\Chats\ChatsService;
 use Livewire\Component;
@@ -16,9 +15,8 @@ class Chatbox extends Component
         $auth_id = auth()->user()->id;
 
         return [
-            "echo-private:chat.{$auth_id},MessageSent" => 'broadcastedMessageReceived',
             "echo-private:chat.{$auth_id},ChatDelete" => 'chatDelete',
-            'loadChat', 'resetChat', 'broadcastMessageRead'
+            'loadChat', 'resetChat'
         ];
     }
 
@@ -44,32 +42,6 @@ class Chatbox extends Component
     public function resetChat(): void
     {
         $this->selectedChat = null;
-    }
-
-    public function broadcastMessageRead(): void
-    {
-        $receivers = $this->getChatsService()->getChatReceivers($this->selectedChat->id, auth()->id())->get();
-
-        if(!$receivers->count()) {
-            return;
-        }
-
-        foreach ($receivers as $receiver){
-            broadcast(new MessageRead($this->selectedChat->id, $receiver->id));
-        }
-    }
-
-    public function broadcastedMessageReceived($event): void
-    {
-        $this->dispatch('refreshChatList');
-
-        if($event['user']['id'] === auth()->id()) {
-            return;
-        }
-
-        if(!$this->selectedChat) {
-            $this->dispatch('notify', ['user' => ['name' => $event['user']['name']]]);
-        }
     }
 
     public function render()
